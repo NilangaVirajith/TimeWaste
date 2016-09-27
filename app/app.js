1f36a558171a209f783108bb9643a35d019144d4
Nilanga Virajith <nvirajith@virtusapolaris.com>
2016-09-27 11:26:33 +0530

    login started

--- app/app.js
+++ app/app.js
@@ -1,3 +1,11 @@
 (function(){
-    angular.module('TimeWaste',[]);
+    angular.module('TimeWaste',['ui.router'])
+        .config($stateProvider){
+            $stateProvider
+                .state('signUp', {
+                url: '',
+                templateUrl: 'app/signup/signup.html',
+                controller: 'SignUpController'
+            })
+    }
 }());
